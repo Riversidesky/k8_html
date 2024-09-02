4cc76846ac5d4294a4eab5c8f72d35783a40323d
RiverSideSky <tnals4637@naver.com>
2024-09-02 17:53:29 +0900

    js

---
+++ 06/06.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bt1 = document.getElementById('bt1');
+    const bt2 = document.getElementById('bt2');
+    const bt3 = document.getElementById('bt3');
+    const bt4 = document.getElementById('bt4');
+    const bt5 = document.getElementById('bt5');
+    const bt6 = document.getElementById('bt6');
+});
